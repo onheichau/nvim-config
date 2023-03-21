fb22bab398e957857aad36b6b15f38ed9c8e0d90
paul chau <paul@chau>
2023-03-21 06:00:42 -0400

    quick

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,2 +1,2 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>f", builtin.find_files, {})
