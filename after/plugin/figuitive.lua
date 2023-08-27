6ae551c4dc12aed3cd053a427474a6f571cdd740
Paul Chau <ochau2@myseneca.ca>
2023-08-26 20:47:40 -0400

    add new git integration plugin

---
+++ after/plugin/figuitive.lua
@@ -0,0 +1,6 @@
+vim.keymap.set("n", "<leader>ga", "<cmd>Git add %<CR>")
+vim.keymap.set(
+	"n",
+	"<leader>gc",
+	":!git commit -m '' && git push<left><left><left><left><left><left><left><left><left><left><left><left><left>"
+)
