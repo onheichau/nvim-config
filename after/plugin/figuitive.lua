567b43b49afe855a714384bd09b21ebbb6bf82e7
Paul Chau <ochau2@myseneca.ca>
2023-09-07 22:17:50 -0400

    grouped the git commit keymap

--- after/plugin/figuitive.lua
+++ after/plugin/figuitive.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "<leader>ga", "<cmd>Git add %<CR>")
-vim.keymap.set("n", "<leader>gc", ":!git commit -m ''<left>")
+--vim.keymap.set("n", "<leader>ga", "<cmd>Git add %<CR>")
+vim.keymap.set("n", "<leader>gc", ":!git add % && git commit -m ''<left>")
