9148ded16edcf7f00e1eb6a16ce6fc36c069d1ee
Paul Chau <ochau2@myseneca.ca>
2023-09-26 10:26:01 -0400

    updated keymap

--- lua/paul/keyremap.lua
+++ lua/paul/keyremap.lua
@@ -2,7 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", ":Ex<CR>")
 vim.keymap.set("n", "<", ":bp<CR>")
 vim.keymap.set("n", ">", ":bn<CR>")
---vim.keymap.set("n", "<Tab>", "2<C-w>", { noremap = true })
 vim.keymap.set("n", "<leader>s", 'yiw:%s/<C-r>"//g<Left><Left>', { noremap = true })
 vim.keymap.set("n", "<leader>=", "=ap", { noremap = true })
 vim.keymap.set("n", "<leader>g", ":DiffviewOpen<CR>", { noremap = true })
@@ -10,12 +9,12 @@ vim.keymap.set("n", "<leader>G", ":DiffviewClose<CR>", { noremap = true })
 vim.keymap.set("n", "<C-n>", "<cmd>noh<CR>")
 --
 vim.keymap.set("i", "<ESC>", "<ESC>l")
-vim.keymap.set("n", "<leader>n", ":w<CR>:bnext<CR>")
 
 vim.keymap.set("i", "<C-l>", "<right>")
 vim.keymap.set("i", "<C-h>", "<left>")
 --
 vim.keymap.set("n", "<C-o>", "<C-o>zz", { noremap = true })
+vim.keymap.set("n", "<C-i>", "<C-i>zz", { noremap = true })
 
 vim.keymap.set("n", "G", "Gzz", { noremap = true })
 
@@ -28,17 +27,14 @@ vim.keymap.set("n", "N", "Nzz", { noremap = true })
 vim.keymap.set("n", "v", "V", { noremap = true })
 vim.keymap.set("n", "V", "v", { noremap = true })
 
-vim.keymap.set("n", "v", "V", { noremap = true })
-vim.keymap.set("n", "V", "v", { noremap = true })
-
 vim.keymap.set("n", "S", "s", { noremap = true })
 vim.keymap.set("n", "s", "S", { noremap = true })
 
 -- Navgation
-vim.keymap.set("n", "<C-j>", "<C-d>zz10<C-e>", { noremap = true })
-vim.keymap.set("n", "<C-k>", "<C-u>zz10", { noremap = true })
-vim.cmd("vnoremap <C-j> 15jzz")
-vim.cmd("vnoremap <C-k> 15kzz")
+vim.keymap.set("n", "<C-j>", "<C-d>", { noremap = true })
+vim.keymap.set("n", "<C-k>", "<C-u>", { noremap = true })
+vim.cmd("vnoremap <C-j> 15j")
+vim.cmd("vnoremap <C-k> 15k")
 
 -- Editiing
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -49,21 +45,9 @@ vim.keymap.set("v", "q:", "q:a")
 vim.keymap.set("n", "q:", "q:a")
 
 -- class name inserter
-vim.keymap.set(
-	"x",
-	"S",
-	[[:s/\v(((^\s*).*\S\s)(\w+.*\s?[(].*[)].*);)/\2\4 {\r\r\3}\r<left><left><left><left><left><left><left><left><left><left><left><left><left>]],
-	{ noremap = true }
-)
-
--- default compile and execute
-vim.keymap.set("n", "<leader>r", function()
-	vim.cmd("!python3 %")
-end)
-
--- vim.cmd("!g++ -std=c++17 -Wall -g -fdiagnostics-color=always *.cpp")
--- vim.cmd("! ./a.out")
-
-vim.keymap.set("n", "<leader>j", function()
-	vim.cmd("ju")
-end)
+-- vim.keymap.set(
+-- 	"x",
+-- 	"S",
+-- 	[[:s/\v(((^\s*).*\S\s)(\w+.*\s?[(].*[)].*);)/\2\4 {\r\r\3}\r<left><left><left><left><left><left><left><left><left><left><left><left><left>]],
+-- 	{ noremap = true }
+-- )
