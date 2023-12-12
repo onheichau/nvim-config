aa0a36fd0aa9b9d2a54ca69fe9ad4f22949fd717
Paul Chau <ochau2@myseneca.ca>
2023-12-12 18:36:48 -0500

    update config

--- after/plugin/figuitive.lua
+++ after/plugin/figuitive.lua
@@ -1,2 +1,6 @@
 --vim.keymap.set("n", "<leader>ga", "<cmd>Git add %<CR>")
-vim.keymap.set("n", "<leader>gc", ":!git add % && git commit -m ''<left>")
+vim.keymap.set(
+	"n",
+	"<leader>gc",
+	":!git add % && git commit -m '' && git push<left><left><left><left><left><left><left><left><left><left><left><left><left>"
+)
